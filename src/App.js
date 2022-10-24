5bf59b4381d5caf3d6075b9df9d020eb721193ff
Ibraheem Uthman <iamlonelypoet@gmail.com>
2022-10-24 13:28:03 +0100

    responsiveness

--- src/App.js
+++ src/App.js
@@ -45,6 +45,26 @@ function App() {
       },
     ],
   };
+
+  const CardWrapper = styled.div`
+    padding-top: 3rem;
+    @media (min-width: 768px) {
+      padding-top: 3rem;
+    }
+    @media (min-width: 425px) {
+      padding-top: 2rem;
+    }
+  `;
+  const Links = styled.div`
+    @media (max-width: 768px) {
+      padding-left: 3rem;
+      padding-right: 3rem;
+    }
+    @media (max-width: 425px) {
+      padding-left: 1rem;
+      padding-right: 1rem;
+    }
+  `;
   const Container = styled.div`
     text-align: center;
   `;
@@ -54,67 +74,95 @@ function App() {
     border-radius: 2px;
     padding: 0.5rem 0;
     margin: 0.5rem 1rem;
-    width: 25rem;
+    width: 35rem;
     color: Black;
     text-decoration: none;
+    @media (max-width: 768px) {
+      width: 25rem;
+    }
+    @media (max-width: 425px) {
+      width: 13rem;
+    }
+  `;
+  const Footer = styled.div`
+    @media (max-width: 768px) {
+      text-align: center;
+    }
+  `;
+  const FooterItem = styled.div`
+    @media (max-width: 768px) {
+      padding: 1rem;
+    }
   `;
   return (
-    <div className="p-m-4 mt-8">
-      <Container>
-        <div className="p-d-flex p-jc-center p-ai-centre p-flex-column p-flex-md-row">
-          <BlockUI>
-            <Avatar
-              className="p-jc-right"
-              image={data.profile__img}
-              size="xlarge"
-              shape="circle"
-            />
-            <h3 className="p-m-2 mb-3">{data.twitter}</h3>
-            <h3 className="p-m-2 mb-3" style={{ display: "none" }}>
-              {data.slack}
-            </h3>
-          </BlockUI>
-        </div>
-      </Container>
+    <CardWrapper>
+      <div className="p-m-4 mt-8">
+        <Container>
+          <div className="p-d-flex p-jc-center p-ai-centre p-flex-column p-flex-md-row">
+            <BlockUI>
+              <Avatar
+                className="p-jc-right"
+                image={data.profile__img}
+                size="xlarge"
+                shape="circle"
+              />
+              <h3 className="p-m-2 mb-3">{data.twitter}</h3>
+              <h3 className="p-m-2 mb-3" style={{ display: "none" }}>
+                {data.slack}
+              </h3>
+            </BlockUI>
+          </div>
+        </Container>
 
-      <div className="p-d-flex p-jc-center p-ai-centre p-flex-column p-flex-md-row">
-        <div className="p-d-flex p-flex-column">
-          {data.links.map((link) => (
-            <Buttons
-              className="p-m-2 p-button-lg p-button-text p-jc-center p-mb-2 p-mr-2"
-              href={link.url}
-              target="_blank"
-              rel="noreferrer"
-              style={{ backgroundColor: "GrayText" }}
-            >
-              {link.name}
-            </Buttons>
-          ))}
+        <div className="p-d-flex p-jc-center p-ai-centre p-flex-column p-flex-md-row">
+          <div className="p-d-flex p-flex-column">
+            {data.links.map((link) => (
+              <Links>
+                <Buttons
+                  className="p-m-2 p-button-lg p-button-text p-jc-center p-mb-2 p-mr-2"
+                  href={link.url}
+                  target="_blank"
+                  rel="noreferrer"
+                  style={{ backgroundColor: "GrayText" }}
+                >
+                  {link.name}
+                </Buttons>
+              </Links>
+            ))}
+          </div>
         </div>
-      </div>
-      <div className="p-d-flex p-jc-center p-ai-centre p-m-4">
-        <Button className="p-m-2 p-button-text">
-          <a href={data.slack} target="_blank" rel="noreferrer">
-            <i className="pi pi-slack" style={{ fontSize: "2em" }}></i>
-          </a>
-        </Button>
-        <Button className="p-m-2 p-button-text">
-          <a href={data.github} target="_blank" rel="noreferrer">
-            <i className="pi pi-github" style={{ fontSize: "2em" }}></i>
-          </a>
-        </Button>
-      </div>
-      <Divider />
-      <div class="p-d-flex p-jc-between  p-flex-column p-flex-md-row">
-        <div className="p-mb-2 p-mr-2">
-          <Image src={data.zuri__img} />
+        <div className="p-d-flex p-jc-center p-ai-centre p-m-4">
+          <Button className="p-m-2 p-button-text">
+            <a href={data.slack} target="_blank" rel="noreferrer">
+              <i className="pi pi-slack" style={{ fontSize: "2em" }}></i>
+            </a>
+          </Button>
+          <Button className="p-m-2 p-button-text">
+            <a href={data.github} target="_blank" rel="noreferrer">
+              <i className="pi pi-github" style={{ fontSize: "2em" }}></i>
+            </a>
+          </Button>
         </div>
-        <div className="p-mb-2 p-mr-2">{data.hng}</div>
-        <div className="p-mb-2 p-mr-2">
-          <Image src={data.i4g__img} />
-        </div>
+        <Divider />
+        <Footer>
+          <div class="p-d-flex p-jc-between  p-flex-column p-flex-md-row">
+            <FooterItem>
+              <div className="p-mb-2 p-mr-2">
+                <Image src={data.zuri__img} />
+              </div>
+            </FooterItem>
+            <FooterItem>
+              <div className="p-mb-2 p-mr-2">{data.hng}</div>
+            </FooterItem>
+            <FooterItem>
+              <div className="p-mb-2 p-mr-2">
+                <Image src={data.i4g__img} />
+              </div>
+            </FooterItem>
+          </div>
+        </Footer>
       </div>
-    </div>
+    </CardWrapper>
   );
 }
 
